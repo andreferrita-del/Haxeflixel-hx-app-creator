6ef09d7146797641dd4cf442ec0d83ede70b17bd
andreferrita-del <andreferrita@gmail.com>
2026-08-22 21:15:45 -0300

    Update ScriptState.hx

--- source/runtime/ScriptState.hx
+++ source/runtime/ScriptState.hx
@@ -29,14 +29,57 @@ class ScriptState extends FlxState {
 
 		try {
 			script = new SScript(fullPath);
+			
+			// Context Injection
 			script.set("state", this);
-			script.set("FlxG", flixel.FlxG);
-			script.set("FlxSprite", flixel.FlxSprite);
-			script.set("FlxText", flixel.text.FlxText);
-			script.set("FlxColor", flixel.util.FlxColor);
 			script.set("ScriptState", ScriptState);
 			script.set("RuntimeLoader", RuntimeLoader);
-			
+
+			// Haxe Core Imports
+			script.set("Math", Math);
+			script.set("Std", Std);
+			script.set("StringTools", StringTools);
+			script.set("Date", Date);
+			script.set("Reflect", Reflect);
+			script.set("Type", Type);
+			script.set("Json", haxe.Json);
+			script.set("File", sys.filesystem.File);
+			script.set("FileSystem", sys.filesystem.FileSystem);
+
+			// Flixel Core Imports
+			script.set("FlxG", flixel.FlxG);
+			script.set("FlxObject", flixel.FlxObject);
+			script.set("FlxSprite", flixel.FlxSprite);
+			script.set("FlxState", flixel.FlxState);
+			script.set("FlxSubState", flixel.FlxSubState);
+			script.set("FlxBasic", flixel.FlxBasic);
+			script.set("FlxCamera", flixel.FlxCamera);
+
+			// Flixel Text & UI
+			script.set("FlxText", flixel.text.FlxText);
+			script.set("FlxButton", flixel.ui.FlxButton);
+			script.set("FlxBar", flixel.ui.FlxBar);
+
+			// Flixel Groups & Tilemaps
+			script.set("FlxGroup", flixel.group.FlxGroup);
+			script.set("FlxTypedGroup", flixel.group.FlxGroup.FlxTypedGroup);
+			script.set("FlxSpriteGroup", flixel.group.FlxSpriteGroup);
+			script.set("FlxTilemap", flixel.tile.FlxTilemap);
+
+			// Flixel Utils & Math
+			script.set("FlxColor", flixel.util.FlxColor);
+			script.set("FlxMath", flixel.math.FlxMath);
+			script.set("FlxPoint", flixel.math.FlxPoint);
+			script.set("FlxRect", flixel.math.FlxRect);
+			script.set("FlxTimer", flixel.util.FlxTimer);
+			script.set("FlxTween", flixel.tweens.FlxTween);
+			script.set("FlxEase", flixel.tweens.FlxEase);
+			script.set("FlxSave", flixel.util.FlxSave);
+
+			// Flixel Audio & Effects
+			script.set("FlxSound", flixel.sound.FlxSound);
+			script.set("FlxEmitter", flixel.effects.particles.FlxEmitter);
+
 			script.execute();
 
 			if (script.exists("onCreate")) {
